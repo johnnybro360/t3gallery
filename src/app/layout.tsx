1543bdd9297fbada674f554ed2d2b834ce68cf03
Jonghun Park <abest82@gmail.com>
2025-07-27 16:27:23 +0800

    Scaffold UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,15 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex w-full justify-between items-center p-4 text-xl font-semibold">
+      <div>Gallery</div>
+      <div>Sign in</div>
+    </nav>
+  )
+}
+
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
@@ -19,7 +28,10 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
+      <body className="flex flex-col gap-4">
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-import Link from "next/link";
+import Image from "next/image";
+
+const mockUrls = [
+"https://ds56rgt372.ufs.sh/f/mZHBdN2dQRYqjwd2vMEv0PVlH9wOyYLi7Ac8qfTeQW6t3nuB",
+"https://ds56rgt372.ufs.sh/f/mZHBdN2dQRYqZJirqEwoYSNGVDUbgMEfmWZ5274nh3TzvyjQ",
+"https://ds56rgt372.ufs.sh/f/mZHBdN2dQRYqMTWup4RQtIPX6DRzkxqAUwWFSmY3B9gpdCJ2"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={`Image ${image.id}`} />
+          </div>
+        ))}
+      </div>
       Hello (gallery in progress)
     </main>
   );
